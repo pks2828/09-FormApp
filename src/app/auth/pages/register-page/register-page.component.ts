c9fae12dea8593af5f4d7edfc20cbbd68f09506f
Angel Doria <danieldoriamoncada28@gmail.com>
2024-02-21 13:42:23 -0600

    Fin Seccion 18

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({//explica esto en el minuto 11:00 de la ultima clase de la seccion 18
+    name: ['', [Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
+    // email: ['', [Validators.required, Validators.pattern (this.validatorsService.emailPattern) ], [ new EmailValidator() ] ],
+    email: ['', [Validators.required, Validators.pattern (this.validatorsService.emailPattern) ], [ this.EmailValidator ] ],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider ]],
+    password: ['', [Validators.required, Validators.minLength(6) ]],
+    password2: ['', [Validators.required ]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo( 'password', 'password2' )
+    ]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private EmailValidator: EmailValidator
+  ) {}
+
+  isValiedField(  field: string ){
+    //TODO: obtener validacion desde un servicio
+    return this.validatorsService.isValidField( this.myForm, field )
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+
 }
+//HASTA AHORITA HEMOS VISTO 3 FORMAS DE REALIZAR VALIDACIONES, MEDIANTE UN SERVICIO SINCRONO, UN SERVICIO ASINCRONO Y MEDIANTE DISTINTOS ARCHIVOS CREANDO FUNCIONES
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,68 @@
+
+
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EmailValidator implements AsyncValidator {
+
+
+  validate(control: AbstractControl ): Observable<ValidationErrors | null> {
+
+    const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+
+      console.log({email});
+
+      if (email === 'fernando@google.com'){
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        //return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+
+    }).pipe(
+      delay( 3000 )
+    )
+
+    return httpCallObservable;
+
+  }
+
+
+
+  // const email = control.value;
+  // console.log({ email });
+
+
+  // return of({
+  //   emailTaken: true
+  // }).pipe(
+  //   delay( 2000 )
+  // )
+
+
+
+
+}
+
+
+/* return this.http.get<any[]>('http://localhost:3000/users?q=${email}')
+  .pipe(
+    delay(3000),
+    map(resp => {
+      return (resp.length === 0 )
+      ? null
+        : { emailTaken: true };
+    })
+  );
+
+ ENDPOINT PARA VERIFICAR CORREO EJEMPLO
+
+
+*/
